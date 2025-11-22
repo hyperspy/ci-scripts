33b19af39588e0048820b47dfc7c2ac20d838383
Eric Prestat <eric.prestat@gmail.com>
2025-11-22 16:16:39 +0000

    Specify environment in the command line and remove it from the environment file to be able to install in current environment more easily

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -2,7 +2,7 @@ ENV_NAME="test_env"
 
 conda config --set channel_priority strict
 mamba create -y --name $ENV_NAME python=$PYTHON_VERSION
-mamba env update --file conda_environment.yml
-mamba env update --file conda_environment_dev.yml
+mamba env update --file conda_environment.yml -n $ENV_NAME
+mamba env update --file conda_environment_dev.yml -n $ENV_NAME
 conda info
 conda list -n $ENV_NAME
